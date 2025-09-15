59157766dc990f245fe997c41ad700c23cf5600c
Rob Vesse <rvesse@apache.org>
2025-09-15 15:34:45 +0100

    Support parsing types with CharSequence conversions (#156)
    Adds support to the DefaultTypeConverter so that when converting it
    considers methods and constructors that take either a String, or a
    CharSequence, for users who want to write more modern/String independent
    code.

--- airline-core/src/main/java/com/github/rvesse/airline/types/DefaultTypeConverter.java
+++ airline-core/src/main/java/com/github/rvesse/airline/types/DefaultTypeConverter.java
@@ -27,20 +27,28 @@ import java.nio.file.Paths;
 /**
  * The default type converter
  * <p>
- * This converter supports all the basic Java types plus types. Additionally it
- * supports any class that defines a static {@code fromString(String)} or
- * {@code valueOf(String)} method. Finally it supports any class that defines a
+ * This converter supports all the basic Java types plus types. Additionally it supports any class that defines a static
+ * {@code fromString(String)} or {@code valueOf(String)} method. Finally it supports any class that defines a
  * constructor that takes a string.
  * </p>
  */
-public class DefaultTypeConverter extends DefaultTypeConverterProvider implements TypeConverter  {
+public class DefaultTypeConverter extends DefaultTypeConverterProvider implements TypeConverter {
 
     private NumericTypeConverter numericConverter;
 
+    /**
+     * Creates a new instance of the default type converter with the default {@link NumericTypeConverter} configured
+     */
     public DefaultTypeConverter() {
         this(null);
     }
 
+    /**
+     * Creates a new instance of the default type converter
+     *
+     * @param numericConverter Numeric type converter to use, if {@code null} then {@link DefaultNumericConverter} is
+     *                         used
+     */
     public DefaultTypeConverter(NumericTypeConverter numericConverter) {
         this.numericConverter = numericConverter != null ? numericConverter : new DefaultNumericConverter();
     }
@@ -51,60 +59,82 @@ public class DefaultTypeConverter extends DefaultTypeConverterProvider implement
 
         // Firstly try the standard Java types
         ConvertResult result = tryConvertBasicTypes(name, type, value);
-        if (result.wasSuccessful())
+        if (result.wasSuccessful()) {
             return result.getConvertedValue();
+        }
 
         // Then look for a static fromString(String) method
         result = tryConvertFromString(name, type, value);
-        if (result.wasSuccessful())
+        if (result.wasSuccessful()) {
             return result.getConvertedValue();
+        }
 
         // Then look for a static valueOf(String) method
         // This covers enums which have a valueOf method
         result = tryConvertFromValueOf(name, type, value);
-        if (result.wasSuccessful())
+        if (result.wasSuccessful()) {
             return result.getConvertedValue();
+        }
 
         // Finally look for a constructor taking a string
         result = tryConvertStringConstructor(name, type, value);
-        if (result.wasSuccessful())
+        if (result.wasSuccessful()) {
             return result.getConvertedValue();
+        }
 
         throw new ParseOptionConversionException(name, value, type.getSimpleName());
     }
 
     /**
      * Checks that the arguments are all non-null
-     * 
-     * @param name
-     *            Option/Argument name
-     * @param type
-     *            Target type
-     * @param value
-     *            String to convert
+     *
+     * @param name  Option/Argument name
+     * @param type  Target type
+     * @param value String to convert
      */
     public static void checkArguments(String name, Class<?> type, String value) {
-        if (name == null)
+        if (name == null) {
             throw new NullPointerException("name is null");
-        if (type == null)
+        }
+        if (type == null) {
             throw new NullPointerException("type is null");
-        if (value == null)
+        }
+        if (value == null) {
             throw new NullPointerException("value is null");
+        }
     }
 
     /**
-     * Tries to convert the value by invoking a constructor that takes a string
-     * on the type
-     * 
-     * @param type
-     *            Type
-     * @param value
-     *            value
+     * Tries to convert the value by invoking a constructor that takes a string on the type
+     * <p>
+     * Considers two variants of the constructor, one which takes {@link String} as its parameter type and if that doesn't
+     * exist one that takes {@link CharSequence} as it's parameter type
+     * </p>
+     *
+     * @param type  Type
+     * @param value value
      * @return Conversion result
      */
     protected final ConvertResult tryConvertStringConstructor(String name, Class<?> type, String value) {
+        ConvertResult result = tryConvertConstructor(name, type, value, String.class);
+        if (result.wasSuccessful()) {
+            return result;
+        } else {
+            return tryConvertConstructor(name, type, value, CharSequence.class);
+        }
+    }
+
+    /**
+     * Tries to convert the value by invoking a constructor that takes a string on the type
+     *
+     * @param type  Type
+     * @param value value
+     * @return Conversion result
+     */
+    protected final <T> ConvertResult tryConvertConstructor(String name, Class<?> type, String value,
+                                                            Class<T> parameterType) {
         try {
-            Constructor<?> constructor = type.getConstructor(String.class);
+            Constructor<?> constructor = type.getConstructor(parameterType);
             return new ConvertResult(constructor.newInstance(value));
         } catch (Throwable ignored) {
         }
@@ -112,13 +142,10 @@ public class DefaultTypeConverter extends DefaultTypeConverterProvider implement
     }
 
     /**
-     * Tries to convert the value by invoking a static {@code valueOf(String)}
-     * method on the type
-     * 
-     * @param type
-     *            Type
-     * @param value
-     *            Value
+     * Tries to convert the value by invoking a static {@code valueOf(String)} method on the type
+     *
+     * @param type  Type
+     * @param value Value
      * @return Conversion result
      */
     protected final ConvertResult tryConvertFromValueOf(String name, Class<?> type, String value) {
@@ -126,13 +153,10 @@ public class DefaultTypeConverter extends DefaultTypeConverterProvider implement
     }
 
     /**
-     * Tries to convert the value by invoking a static
-     * {@code fromString(String)} method on the type
-     * 
-     * @param type
-     *            Type
-     * @param value
-     *            Value
+     * Tries to convert the value by invoking a static {@code fromString(String)} method on the type
+     *
+     * @param type  Type
+     * @param value Value
      * @return Conversion result
      */
     protected final ConvertResult tryConvertFromString(String name, Class<?> type, String value) {
@@ -141,34 +165,53 @@ public class DefaultTypeConverter extends DefaultTypeConverterProvider implement
     }
 
     /**
-     * Tries to convert the value by invoking a static method on the type
-     * 
-     * @param type
-     *            Type
-     * @param value
-     *            Value
-     * @param methodName
-     *            Name of the method to invoke
+     * Tries to convert the value by invoking a static method on the
+     * <p>
+     * Considers two variants of the method, one which takes {@link String} as its parameter type and if that doesn't
+     * exist one that takes {@link CharSequence} as it's parameter type
+     * </p>
+     *
+     * @param type       Type
+     * @param value      Value
+     * @param methodName Name of the method to invoke
      * @return Conversion Result
      */
+    @SuppressWarnings("unused")
     protected final ConvertResult tryConvertStringMethod(String name, Class<?> type, String value, String methodName) {
+        ConvertResult result = tryConvertStringMethod(type, value, methodName, String.class);
+        if (result.wasSuccessful()) {
+            return result;
+        } else {
+            return tryConvertStringMethod(type, value, methodName, CharSequence.class);
+        }
+    }
+
+    /**
+     * Tries to convert the value by invoking a static method on the type
+     *
+     * @param type       Type
+     * @param value      Value
+     * @param methodName Name of the method to invoke
+     * @return Conversion Result
+     */
+    protected final ConvertResult tryConvertStringMethod(Class<?> type, String value,
+                                                         String methodName, Class<?> parameterType) {
         try {
-            Method method = type.getMethod(methodName, String.class);
+            Method method = type.getMethod(methodName, parameterType);
             if (method.getReturnType().isAssignableFrom(type)) {
                 return new ConvertResult(method.invoke(null, value));
             }
         } catch (Throwable ignored) {
+
         }
         return ConvertResult.FAILURE;
     }
 
     /**
      * Tries to convert the value if it is one of the common Java types
-     * 
-     * @param type
-     *            Type
-     * @param value
-     *            Value
+     *
+     * @param type  Type
+     * @param value Value
      * @return Conversion result
      */
     protected final ConvertResult tryConvertBasicTypes(String name, Class<?> type, String value) {
